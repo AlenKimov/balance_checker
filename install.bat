b4e1b358dbd6f50bafaa8031a814966247d5a967
AlenKimov <alen.kimov@gmail.com>
2022-11-09 01:26:43 +0300

    Создание папки с настройками при запуске install.bat

--- install.bat
+++ install.bat
@@ -5,6 +5,8 @@ python -m venv .venv
 call .venv\Scripts\activate.bat
 @echo Installing libraries...
 pip install -r requirements.txt
+@echo Creating settings files...
+python balance_checker\config.py
 @echo To start the script run "start.bat"
 pause
 :: start.bat
