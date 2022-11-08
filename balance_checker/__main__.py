7e0f5a1af8a350aeea1b5e2e11b6bb59b704f704
AlenKimov <alen.kimov@gmail.com>
2022-11-09 02:04:15 +0300

    Учет навалидных строк.

--- balance_checker/__main__.py
+++ balance_checker/__main__.py
@@ -3,18 +3,25 @@ from eth_account.signers.local import LocalAccount
 
 # Скрипты проекта
 from config import addresses, private_keys, mnemonics
+from logger import logger
 from debank_api import get_address_balances
 from output_funcs import save_to_output
 
 
 def main():
     for private_key in private_keys:
-        account: LocalAccount = Account.from_key(private_key=private_key)
-        addresses.append(account.address)
+        try:
+            account: LocalAccount = Account.from_key(private_key=private_key)
+            addresses.append(account.address)
+        except Exception:
+            logger.warning(f"[{private_key}] Неверный приватный ключ")
 
     for mnemonic in mnemonics:
-        account: LocalAccount = Account.from_mnemonic(mnemonic=mnemonic)
-        addresses.append(account.address)
+        try:
+            account: LocalAccount = Account.from_mnemonic(mnemonic=mnemonic)
+            addresses.append(account.address)
+        except Exception:
+            logger.warning(f"[{mnemonic}] Неверная мнемоническая фраза")
 
     save_to_output(get_address_balances(addresses))
 
