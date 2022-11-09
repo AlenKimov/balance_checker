29e6e58b32c644fedf348fadefbe955b946a7010
AlenKimov <alen.kimov@gmail.com>
2022-11-09 05:10:45 +0300

    Починил.

--- balance_checker/__main__.py
+++ balance_checker/__main__.py
@@ -1,29 +1,38 @@
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
+import eth_utils
 
 # Скрипты проекта
-from config import addresses, private_keys, mnemonics
+from config import addresses, private_keys, mnemonics, settings
 from logger import logger
-from debank_api import get_address_balances
-from output_funcs import save_to_output
+from model import AddressData
+from debank_api import update_data_list_address_balances
+from output import save_data_list_to_output
 
 
 def main():
+    Account.enable_unaudited_hdwallet_features()
+
+    data: list[AddressData] = list()
+    for address in addresses:
+        data.append(AddressData(address=address))
+
     for private_key in private_keys:
         try:
             account: LocalAccount = Account.from_key(private_key=private_key)
-            addresses.append(account.address)
-        except Exception:
-            logger.warning(f"[{private_key}] Неверный приватный ключ")
+            data.append(AddressData(address=account.address, private_key=private_key))
+        except Exception:  # binascii.Error
+            logger.error(f"[{private_key}] Неверный приватный ключ")
 
     for mnemonic in mnemonics:
         try:
             account: LocalAccount = Account.from_mnemonic(mnemonic=mnemonic)
-            addresses.append(account.address)
-        except Exception:
-            logger.warning(f"[{mnemonic}] Неверная мнемоническая фраза")
+            data.append(AddressData(address=account.address, mnemonic=mnemonic))
+        except eth_utils.exceptions.ValidationError:
+            logger.error(f"[{mnemonic}] Неверная мнемоническая фраза")
 
-    save_to_output(get_address_balances(addresses))
+    update_data_list_address_balances(data, timeout=settings.timeout)
+    save_data_list_to_output(data)
 
 
 if __name__ == '__main__':
--- balance_checker/debank_api.py
+++ balance_checker/debank_api.py
@@ -11,39 +11,36 @@ from model import AddressData
 from utils import cut_list
 
 
-def get_address_balances(addresses: list[str], limit: int = 8, timeout: int = 21) -> list[AddressData]:
+def update_data_list_address_balances(data_list: list[AddressData], limit: int = 8, timeout: int = 60):
     logger.info("Пытаюсь запросить балансы адресов..")
-    data_list: list[AddressData] = list()
 
     url = 'https://api.debank.com/user/addr'
 
-    async def _get_usd_value(session: ClientSession, address: str):
-        response = await session.get(url, params={'addr': address})
+    async def _get_usd_value(session: ClientSession, data: AddressData):
+        response = await session.get(url, params={'addr': data.address})
         try:
             response_json = await response.json()
-            usd_value = response_json["data"].get('usd_value', 0)
-            data_list.append(AddressData(address=address, balance=usd_value))
-            logger.success(f"[{address}] Баланс: ${usd_value:.1f}")
+            data.balance = response_json["data"].get('usd_value', 0)
+            logger.success(f"[{data.address}] Баланс: ${data.balance:.1f}")
         except Exception:
-            logger.error(f"[{address}] Не удалось запросить баланс")
+            logger.error(f"[{data.address}] Не удалось запросить баланс")
 
-    async def _get_address_balances(addresses: list[str]):
+    async def _get_address_balances(data_list: list[AddressData]):
         headers = {'user-agent': random_useragent()}
         async with aiohttp.ClientSession(headers=headers) as session:
             tasks = []
-            for address in addresses:
-                tasks.append(asyncio.create_task(_get_usd_value(session, address)))
+            for data in data_list:
+                tasks.append(asyncio.create_task(_get_usd_value(session, data)))
             for task in tasks:
                 await task
             await asyncio.gather(*tasks)
 
-    addresses_parts = cut_list(addresses, limit)
-    if len(addresses_parts) < 2:
+    data_list_parts = cut_list(data_list, limit)
+    if len(data_list_parts) < 2:
         timeout = 0
 
-    for addresses_part in tqdm(addresses_parts):
-        asyncio.run(_get_address_balances(addresses_part))
+    for data_list_part in tqdm(data_list_parts):
+        asyncio.run(_get_address_balances(data_list_part))
         if timeout > 0:
             logger.info(f"Жду {timeout} секунд..")
             sleep(timeout)
-    return data_list
