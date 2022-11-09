ec80091e6100b6394ede2998e3102bc19ea16708
AlenKimov <alen.kimov@gmail.com>
2022-11-09 05:09:33 +0300

    Новый подход к реализации данного скрипта.

--- balance_checker/config.py
+++ balance_checker/config.py
@@ -1,24 +1,42 @@
+from pydantic import BaseModel
 from os import makedirs
+from pathlib import Path
+import toml
 
-from definitions import SETTINGS_DIR,PUBLIC_KEYS_TXT, PRIVATE_KEYS_TXT, MNEMONICS_TXT
+# Скрипты проекта
+from definitions import SETTINGS_DIR
 
-
-with open(PUBLIC_KEYS_TXT) as file:
-    addresses = [address.strip() for address in file]
-
-with open(PRIVATE_KEYS_TXT) as file:
-    private_keys = [private_key.strip() for private_key in file]
-
-with open(MNEMONICS_TXT) as file:
-    mnemonics = [mnemonic.strip() for mnemonic in file]
-
-
-def create_settings_files():
-    makedirs(SETTINGS_DIR, exist_ok=True)
-    with open(PUBLIC_KEYS_TXT, 'a'): pass
-    with open(PRIVATE_KEYS_TXT, 'a'): pass
-    with open(MNEMONICS_TXT, 'a'): pass
+DOTENV_FILE      = Path(SETTINGS_DIR, '.env')
+SETTINGS_TOML    = Path(SETTINGS_DIR, 'settings.toml')
+PUBLIC_KEYS_TXT  = Path(SETTINGS_DIR, 'public_keys.txt')
+PRIVATE_KEYS_TXT = Path(SETTINGS_DIR, 'private_keys.txt')
+MNEMONICS_TXT    = Path(SETTINGS_DIR, 'mnemonics.txt')
 
 
-if __name__ == '__main__':
-    create_settings_files()
+class Settings(BaseModel):
+    timeout: int = 60
+
+
+settings = Settings()
+
+# Создаю файлы с настройками по умолчанию
+makedirs(SETTINGS_DIR, exist_ok=True)
+with open(PUBLIC_KEYS_TXT, 'a'): pass
+with open(PRIVATE_KEYS_TXT, 'a'): pass
+with open(MNEMONICS_TXT, 'a'): pass
+if not SETTINGS_TOML.exists():
+    with open(SETTINGS_TOML, 'w', encoding='utf-8') as settings_toml:
+        toml.dump(settings.dict(), settings_toml)
+
+
+with open(SETTINGS_TOML, encoding='utf-8') as settings_toml:
+    settings = Settings(**toml.load(settings_toml))
+
+with open(PUBLIC_KEYS_TXT) as public_keys_txt:
+    addresses = [address.strip() for address in public_keys_txt]
+
+with open(PRIVATE_KEYS_TXT) as private_keys_txt:
+    private_keys = [private_key.strip() for private_key in private_keys_txt]
+
+with open(MNEMONICS_TXT) as mnemonics_txt:
+    mnemonics = [mnemonic.strip() for mnemonic in mnemonics_txt]
--- balance_checker/definitions.py
+++ balance_checker/definitions.py
@@ -1,11 +1,6 @@
 from pathlib import Path
 
-ROOT_DIR         = Path(__file__).parent
-LOG_DIR          = Path(ROOT_DIR,     "log")
-OUTPUT_DIR       = Path(ROOT_DIR,     "output")
-SETTINGS_DIR     = Path(ROOT_DIR,     "settings")
-DOTENV_FILE      = Path(SETTINGS_DIR, ".env")
-SETTINGS_TOML    = Path(SETTINGS_DIR, "settings.toml")
-PUBLIC_KEYS_TXT  = Path(SETTINGS_DIR, "public_keys.txt")
-PRIVATE_KEYS_TXT = Path(SETTINGS_DIR, "private_keys.txt")
-MNEMONICS_TXT    = Path(SETTINGS_DIR, "mnemonics.txt")
+ROOT_DIR     = Path(__file__).parent
+LOG_DIR      = Path(ROOT_DIR, 'log')
+OUTPUT_DIR   = Path(ROOT_DIR, 'output')
+SETTINGS_DIR = Path(ROOT_DIR, 'settings')
